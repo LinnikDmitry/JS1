b370234733c9bd1183cc08d8b842ba68bd318002
Dmitry <linnik_1990@mail.ru>
2018-03-26 10:03:57 +0200

    Первый коммит

---
+++ lesson_1/script.js
@@ -0,0 +1,25 @@
+var budget = prompt ("Ваш бюджет?");
+	nameOfShop = prompt ("Название вашего магазина?");	
+	shopGoodsFirst = prompt("Какой тип товаров будем продавать?");
+	shopGoodsSecond = prompt("Какой тип товаров будем продавать?");
+	shopGoodsThird = prompt("Какой тип товаров будем продавать?");
+	shopGoods = [shopGoodsFirst, shopGoodsSecond, shopGoodsThird];
+	open = "OPEN";
+	budgetOnDay = budget / 30;
+	employers = {
+		sellerA: "Jo",
+		sellerB: "John",
+		sellerC: "Johnny"
+	}
+
+	mainList = {
+		budget,
+		nameOfShop,
+		shopGoods,
+		employers,
+		open
+	}
+
+alert('Бюджет на 1 день:   ' + budgetOnDay);
+
+console.log(mainList);
